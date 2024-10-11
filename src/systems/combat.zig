4720eb8aa9b23966d7082182316d18eb4f170dce
avokadoen <akselhallajs@gmail.com>
2024-10-11 11:23:56 +0200

    use acceleration structure for projectiles as well

--- src/systems/combat.zig
+++ src/systems/combat.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const rl = @import("raylib");
 const tracy = @import("ztracy");
 const ecez = @import("ecez");
@@ -101,31 +103,15 @@ pub fn Create(Storage: type) type {
             }
         }
 
-        const ProjectileQuery = Storage.Query(
-            struct {
-                entity: ecez.Entity,
-                pos: components.Position,
-                vel: components.Velocity,
-                circle: components.CircleCollider,
-                proj: components.Projectile,
-            },
-            .{components.InactiveTag},
-        );
-        const QueryKillable = Storage.Query(
-            struct {
-                entity: ecez.Entity,
-                pos: components.Position,
-                col: components.RectangleCollider,
-                health: *components.Health,
-            },
-            .{components.InactiveTag},
-        );
         const ProjectileHitReadView = Storage.Subset(
             .{
                 components.Position,
                 components.Scale,
                 components.Camera,
                 components.Vocals,
+                components.CircleCollider,
+                components.RectangleCollider,
+                components.Projectile,
             },
             .read_only,
         );
@@ -133,12 +119,11 @@ pub fn Create(Storage: type) type {
             .{
                 components.InactiveTag,
                 components.Velocity,
+                components.Health,
             },
             .read_and_write,
         );
         pub fn projectileHitKillable(
-            proj_iter: *ProjectileQuery,
-            killable_iter: *QueryKillable,
             read_view: *ProjectileHitReadView,
             write_view: *ProjectileHitWriteView,
             context: Context,
@@ -152,58 +137,98 @@ pub fn Create(Storage: type) type {
                 cam: components.Camera,
             }) catch unreachable;
 
-            while (proj_iter.next()) |projectile| {
-                defer killable_iter.reset();
-
-                const offset = rl.Vector2.init(
-                    @floatCast(projectile.circle.x),
-                    @floatCast(projectile.circle.y),
-                );
-                const proj_pos = components.Position{
-                    .vec = projectile.pos.vec.add(offset),
-                };
-
-                var extra_dmg: i32 = 0;
-                var has_piercing: bool = false;
-                for (projectile.proj.modifiers[0..projectile.proj.modifier_len]) |mod| {
-                    switch (mod) {
-                        .piercing => has_piercing = true,
-                        .dmg_amp => extra_dmg += 10,
-                    }
+            leaf_loop: for (context.collision_as.leaf_node_storage.items) |leaf_node| {
+                if (leaf_node.isActive() == false) {
+                    continue :leaf_loop;
                 }
-                const pan = ((projectile.pos.vec.x - camera.pos.vec.x) + 0.5) / camera.cam.resolution.x;
 
-                while (killable_iter.next()) |killable| {
-                    if (physics.Intersection.circleAndRect(
-                        projectile.circle,
-                        proj_pos,
-                        killable.col,
-                        killable.pos,
-                    )) {
-                        const maybe_vocals = read_view.getComponent(killable.entity, components.Vocals) catch null;
-                        if (maybe_vocals) |vocals| {
-                            const on_dmg_index = context.rng.intRangeAtMost(u8, vocals.on_dmg_start, vocals.on_dmg_end);
-                            const on_dmg_sound = context.sound_repo[on_dmg_index];
+                proj_loop: for (leaf_node.circle_movable_entities.items) |a| {
+                    // We assume all circle colliders in the game is projectiles currently.
+                    std.debug.assert(read_view.hasComponents(a, .{components.Projectile}));
 
-                            const pitch_range = 0.2;
-                            const pitch = 1 - (context.rng.float(f32) - 0.5) * pitch_range;
-                            rl.setSoundPitch(on_dmg_sound, pitch);
-                            rl.setSoundPan(on_dmg_sound, pan);
-                            rl.playSound(on_dmg_sound);
+                    const projectile = .{
+                        .pos = read_view.getComponent(a, components.Position) catch unreachable,
+                        .col = read_view.getComponent(a, components.CircleCollider) catch unreachable,
+                        .vel = write_view.getComponent(a, components.Velocity) catch unreachable,
+                        .proj = read_view.getComponent(a, components.Projectile) catch unreachable,
+                    };
+
+                    const offset = rl.Vector2.init(
+                        @floatCast(projectile.col.x),
+                        @floatCast(projectile.col.y),
+                    );
+                    const proj_pos = components.Position{
+                        .vec = projectile.pos.vec.add(offset),
+                    };
+
+                    var extra_dmg: i32 = 0;
+                    var has_piercing: bool = false;
+                    for (projectile.proj.modifiers[0..projectile.proj.modifier_len]) |mod| {
+                        switch (mod) {
+                            .piercing => has_piercing = true,
+                            .dmg_amp => extra_dmg += 10,
                         }
+                    }
 
-                        const maybe_vel = write_view.getComponent(killable.entity, *components.Velocity) catch null;
-                        if (maybe_vel) |kill_vel| {
-                            const proj_dir = projectile.vel.vec.normalize();
-                            const proj_impact = rl.Vector2.init(projectile.proj.weight, projectile.proj.weight);
-                            kill_vel.vec = kill_vel.vec.add(proj_dir.multiply(proj_impact));
+                    const pan = ((projectile.pos.vec.x - camera.pos.vec.x) + 0.5) / camera.cam.resolution.x;
+
+                    for (leaf_node.immovable_entities.items) |b| {
+                        const immovable = .{
+                            .pos = read_view.getComponent(b, components.Position) catch unreachable,
+                            .col = read_view.getComponent(b, components.RectangleCollider) catch unreachable,
+                        };
+
+                        if (physics.Intersection.circleAndRect(
+                            projectile.col,
+                            proj_pos,
+                            immovable.col,
+                            immovable.pos,
+                        )) {
+                            // Assumption: objects without velocity does not have a health component
+                            write_view.setComponents(a, .{components.InactiveTag{}}) catch @panic("oom");
+                            continue :proj_loop;
                         }
+                    }
 
-                        killable.health.value -= projectile.proj.dmg + extra_dmg;
+                    for (leaf_node.rect_movable_entities.items) |b| {
+                        // Assumption: All movable are killable
+                        const killable = .{
+                            .pos = read_view.getComponent(b, components.Position) catch unreachable,
+                            .col = read_view.getComponent(b, components.RectangleCollider) catch unreachable,
+                            .health = write_view.getComponent(b, *components.Health) catch unreachable,
+                        };
 
-                        if (!has_piercing) {
-                            write_view.setComponents(projectile.entity, .{components.InactiveTag{}}) catch @panic("oom");
-                            break;
+                        if (physics.Intersection.circleAndRect(
+                            projectile.col,
+                            proj_pos,
+                            killable.col,
+                            killable.pos,
+                        )) {
+                            const maybe_vocals = read_view.getComponent(b, components.Vocals) catch null;
+                            if (maybe_vocals) |vocals| {
+                                const on_dmg_index = context.rng.intRangeAtMost(u8, vocals.on_dmg_start, vocals.on_dmg_end);
+                                const on_dmg_sound = context.sound_repo[on_dmg_index];
+
+                                const pitch_range = 0.2;
+                                const pitch = 1 - (context.rng.float(f32) - 0.5) * pitch_range;
+                                rl.setSoundPitch(on_dmg_sound, pitch);
+                                rl.setSoundPan(on_dmg_sound, pan);
+                                rl.playSound(on_dmg_sound);
+                            }
+
+                            const maybe_vel = write_view.getComponent(b, *components.Velocity) catch null;
+                            if (maybe_vel) |kill_vel| {
+                                const proj_dir = projectile.vel.vec.normalize();
+                                const proj_impact = rl.Vector2.init(projectile.proj.weight, projectile.proj.weight);
+                                kill_vel.vec = kill_vel.vec.add(proj_dir.multiply(proj_impact));
+                            }
+
+                            killable.health.value -= projectile.proj.dmg + extra_dmg;
+
+                            if (!has_piercing) {
+                                write_view.setComponents(a, .{components.InactiveTag{}}) catch @panic("oom");
+                                continue :proj_loop;
+                            }
                         }
                     }
                 }
